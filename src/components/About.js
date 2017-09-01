6a58cf2ce7202f2e4ed27353bbb49417a7ce04f7
quanchifootball <jasonkeum@gmail.com>
2017-08-31 20:09:44 -0700

    edit bio

--- src/components/About.js
+++ src/components/About.js
@@ -11,9 +11,9 @@ export default class About extends React.Component {
                     <h3><strong>Hi</strong>,</h3>
                     <p>I am an aspiring full stack developer currently residing in Vancouver. Just a few years back I was working as a trial lawyer in New York, where I represented clients in labour and employment matters. Practicing law was a rewarding gig, and I got to use cool phrases like <em>double jeopardy</em> and <em>beyond a reasonable doubt</em>. <strong>*</strong> </p>
                     <br></br>
-                    <p>My goal is to get involved in developing solutions that combine law and technology. I have the law part down and am now working on the technology side. Although I’ve taken some college courses on algorithms, systems analysis and OOP, most of what I know is self-taught.</p>
+                    <p>My goal is to get involved in developing practical web applications for business end users. As a long time power user of dozens of B2B applications, I have a lot of insight in this area.</p>
                     <br></br>
-                    <p>This page was built using Node.js with React components made browser-friendly using Gulp, and features some of the projects I’ve built so far. Enjoy, and shoot me a message on Linkedin if you’d like to chat.</p>
+                    <p>Although I’ve taken some college courses on algorithms, systems analysis and OOP, most of what I know is self-taught. This page was built using Node.js with React components made browser-friendly using Gulp, and features some of the projects I’ve built so far. Enjoy, and shoot me a message on Linkedin if you’d like to chat.</p>
                     <br></br>
                     <p className="text-right signed">-Jason</p>
                     <p id="postscript"><strong>*</strong> I was not a criminal lawyer so I guess I never actually used those phrases, but they’re still fun to think about.</p>
