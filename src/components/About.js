16e45e7bb1e6ca8e8fab711fb0173af3e96f3f7a
quanchifootball <jasonkeum@gmail.com>
2017-09-05 15:20:57 -0700

    edit biography

--- src/components/About.js
+++ src/components/About.js
@@ -9,15 +9,13 @@ export default class About extends React.Component {
                 </h2>
                 <div id="about-me">
                     <h3><strong>Hi</strong>,</h3>
-                    <p>I am an aspiring full stack developer currently residing in Vancouver. Just a few years back I was working as a trial lawyer in New York, where I represented clients in labour and employment matters. Practicing law was a rewarding gig, and I got to use cool phrases like <em>double jeopardy</em> and <em>beyond a reasonable doubt</em>. <strong>*</strong> </p>
+                    <p>I am a detail oriented problem solver with American legal experience and a new full-stack web developer seeking an entry level role with a B2B or B2C tech startup. As a long time power user of a variety of business applications, I am interested in developing practical solutions that strongly consider the needs of the end user. No unnecessary bells and whistles, just less time wasted at the office.</p>
                     <br></br>
-                    <p>My goal is to get involved in developing practical web applications for business end users. As a long time power user of dozens of B2B applications, I have a lot of insight in this area.</p>
+                    <p>I started programming in May 2016 knowing absolutely zero about development. Through initiative and disciplined self-learning I can now build and deploy MERN-stack single page apps from scratch!</p>
                     <br></br>
-                    <p>Although I’ve taken some college courses on algorithms, systems analysis and OOP, most of what I know is self-taught. This page was built using Node.js with React components made browser-friendly using Gulp, and features some of the projects I’ve built so far. Enjoy, and shoot me a message on Linkedin if you’d like to chat.</p>
+                    <p>Although I’ve taken some college courses on algorithms, systems analysis and OOP (Java), most of what I know is self-taught. This page was built using Node.js with React components made browser-friendly using Gulp, and features some of the projects I’ve built so far. Enjoy, and shoot me a message on Linkedin if you’d like to chat.</p>
                     <br></br>
-                    <p className="text-right signed">-Jason</p>
-                    <p id="postscript"><strong>*</strong> I was not a criminal lawyer so I guess I never actually used those phrases, but they’re still fun to think about.</p>
-                    
+                    <p className="text-right signed">-Jason</p>                  
                 </div>
             </div>  
         )
