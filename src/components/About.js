1e6b9494ead1b00ec01325568bc9b8adb56f7b3b
quanchifootball <jasonkeum@gmail.com>
2017-09-06 01:07:07 -0700

    rebundle js after updating bio again

--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,13 @@ export default class About extends React.Component {
                 </h2>
                 <div id="about-me">
                     <h3><strong>Hi</strong>,</h3>
-                    <p>I am a detail oriented problem solver with American legal experience and a new full-stack web developer seeking a growing role with a B2B or B2C tech startup. As a long time power user of a variety of business applications, I am interested in developing practical solutions that strongly consider the needs of the end user. No unnecessary bells and whistles, just less time wasted at the office.</p>
+                    <p>I am a detail oriented problem solver and a new full-stack web developer seeking a growing role with a B2B or B2C tech startup. Formerly I worked as lawyer in New York focusing on labour and employment litigation and e-discovery. As a long time power user of a variety of business applications, I am interested in developing practical solutions that strongly consider the needs of the end user.</p>
                     <br></br>
-                    <p>I started programming in May 2016 knowing absolutely zero about development. Through initiative and disciplined self-learning I can now build and deploy MERN-stack single page apps from scratch!</p>
+                    <p>I started programming in May 2016 knowing absolutely zero about development. Through initiative and disciplined self-learning (and patience) I can now build and deploy MERN-stack single page apps from scratch! I’ve taken some college courses on algorithms, systems analysis and OOP (Java), but most of what I know is self-taught. While I still have much more to learn, I am enjoying every minute of the journey.</p>
                     <br></br>
-                    <p>Although I’ve taken some college courses on algorithms, systems analysis and OOP (Java), most of what I know is self-taught. This page was built using Node.js with React components made browser-friendly using Gulp, and features some of the projects I’ve built so far. Enjoy, and shoot me a message on Linkedin if you’d like to chat.</p>
+                    <p>Feel free to <a href="https://www.linkedin.com/messaging/compose/" target="_blank">shoot me a message</a> if you'd like to connect over all things tech. Cheers!</p>
                     <br></br>
-                    <p className="text-right signed">-Jason</p>                  
+                    <p className="text-right signed">-  Jason</p>                  
                 </div>
             </div>  
         )
